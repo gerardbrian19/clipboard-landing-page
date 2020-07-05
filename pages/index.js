31ebafc630dfe1564f9d6990c0e6a20ea57bb7fb
GGueco <43228146+gerardbrian19@users.noreply.github.com>
2020-07-05 14:05:52 +0800

    added styles

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,177 @@
+import React, {Component} from 'react'
 import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Index extends Component {
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    render() {
+        return (
+            <div id="App">
+                <Head>
+                    <title>Frontend Mentor | clipboard-landing-page</title>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png"/>
+                    <link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree:400,600&display=swap" rel="stylesheet"/>
+                </Head>
+                <main id="main-content">
+                    <header className="header">
+                        <div className="container container--header">
+                            <img src="/static/images/logo.svg" alt="Copy Logo" className="header__logo"/>
+                            <h1 className="header__title ">
+                                A history of everything you copy</h1>
+                            <p className="header__description text-gray">Clipboard allows you to track and organize everything you 
+                                        copy. Instantly access your clipboard on all your devices.</p>
+                            <div className="header__buttons">
+                                <a href="" className="btn btn--green">Download for iOS</a>
+                                <a href="" className="btn btn--blue">Download for Mac</a>
+                            </div>
+                        </div>
+                    </header>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                    <section id="features">
+                        <div className="container padbot">
+                            <h2 className="primary-text">Keep track of your snippets</h2>
+                            <p className="support-text text-gray">
+                                Clipboard instantly stores any item you copy in the cloud, 
+                                        meaning you can access your snippets immediately on all your 
+                                        devices. Our Mac and iOS apps will help you organize everything.</p>
+                        </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                        <div className="wide-container">
+                            <div className="wide-container__hero">
+                                <img src="/static/images/image-computer.png" alt="" className="hero__img"/>
+                            </div>
+                            <div className="wide-container__content">
+                                <h3 className="small-title">Quick Search</h3>
+                                <p className="small-title__desc text-gray">
+                                    Easily search your snippets by content, category, web address, application, and more.
+                                </p>
+                                <h3 className="small-title">iCloud Sync</h3>
+                                <p className="small-title__desc text-gray">
+                                    Instantly saves and syncs snippets across all your devices.
+                                </p>
+                                <h3 className="small-title">Complete History</h3>
+                                <p className="small-title__desc text-gray">
+                                    Retrieve any snippets from the first moment you started using the app.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                    <section id="devices" className="padbot">
+                        <h2 className="primary-text">
+                            Access Clipboard anywhere</h2>
+                        <p className="support-text text-gray padbot">
+                            Whether you’re on the go, or at your computer, you can access all your Clipboard 
+                                  snippets in a few simple clicks.
+                        </p>
+                        <div className="container">
+                            <img src="/static/images/image-devices.png" alt="Devices supported" className="devices__img"/>
+                        </div>
+                    </section>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                    <section id="companies" className="padbot">
+                        <h2 className="primary-text">
+                            Supercharge your workflow</h2>
+                        <p className="support-text text-gray padbot">We’ve got the tools to boost your productivity.</p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                        <div className="container container--icons">
+                            <div className="icon__wrapper">
+                                <img src="/static/images/icon-text.svg" alt="blacklist icon" className="icon__img"/>
+                                <h3 className="icon-title">Create Blacklists</h3>
+                                <p className="icon-title__desc">
+                                    Ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
+                            </div>
+                            <div className="icon__wrapper">
+                                <img src="/static/images/icon-blacklist.svg" alt="blacklist icon" className="icon__img"/>
+                                <h3 className="icon-title">
+                                    Plain text snippets</h3>
+                                <p className="icon-title__desc">
+                                    Remove unwanted formatting from copied text for a consistent look.
+                                </p>
+                            </div>
+                            <div className="icon__wrapper">
+                                <img src="/static/images/icon-preview.svg" alt="blacklist icon" className="icon__img"/>
+                                <h3 className="icon-title">Sneak Preview</h3>
+                                <p className="icon-title__desc">
+                                    Quick preview of all snippets on your Clipboard for easy access.          .</p>
+                            </div>
+                        </div>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+                        <div className="container container--companies">
+                            <img src="/static/images/logo-google.png" alt="Company Logo" className="company-logo"/>
+                            <img src="/static/images/logo-ibm.png" alt="Company Logo" className="company-logo"/>
+                            <img src="/static/images/logo-microsoft.png" alt="Company Logo" className="company-logo"/>
+                            <img src="/static/images/logo-hp.png" alt="Company Logo" className="company-logo"/>
+                            <img src="/static/images/logo-vector-graphics.png" alt="Company Logo" className="company-logo"/>
+                        </div>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                        <div className="container container--clipboard">
+                            <h2 className="primary-text">
+                                Clipboard for iOS and Mac OS</h2>
+                            <p className="support-text text-gray">
+                                Available for free on the App Store. Download for Mac or iOS, sync with iCloud 
+                                        and you’re ready to start adding to your clipboard.</p>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+                            <div className="header__buttons">
+                                <a href="" className="btn btn--green">Download for iOS</a>
+                                <a href="" className="btn btn--blue">Download for Mac</a>
+                            </div>
+                        </div>
+                    </section>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                    <footer id="footer">
+                        <div className="container container--footer">
+                            <img src="/static/images/logo.svg" alt="Footer logo" className="footer-logo"/>
+                            <ul className="footer__items">
+                                <li>
+                                    <a href="" className="footer__links">FAQs</a>
+                                </li>
+                                <li>
+                                    <a href="" className="footer__links">Contact Us</a>
+                                </li>
+                            </ul>
+                            <ul className="footer__items">
+                                <li>
+                                    <a href="" className="footer__links">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="" className="footer__links">Press Kit</a>
+                                </li>
+                            </ul>
+                            <ul className="footer__items up">
+                                <li>
+                                    <a href="" className="footer__links">Install Guide</a>
+                                </li>
+                            </ul>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+                            <ul className="footer__items social">
+                                <li>
+                                    <a href="" className="footer__links">
+                                        <i className="fab fa-facebook-square fa-2x"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" className="footer__links">
+                                        <i className="fab fa-twitter fa-2x"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" className="footer__links">
+                                        <i className="fab fa-instagram fa-2x"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </footer>
+                    <p className="attribution">
+                        Challenge by
+                        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                                Coded by
+                        <a href="#">Norberto Ignacio</a>.
+                    </p>
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+                </main>
+            </div>
+        )
+    }
 }
